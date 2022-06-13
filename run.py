a607a61965dd6d6155daea51b540f4136968fc8d
petrugio <petruchelban@gmail.com>
2022-06-13 18:55:01 +0000

    Install and import dependencies

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import yfinance as yf
+import datetime
+from datetime import date
+import pandas as pd
+import numpy as np
+import colorama as cm
+from termcolor import colored
+import os
+from tabulate import tabulate
+
