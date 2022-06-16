574bca27f922b02091478d41d10d75ed96dd8131
petrugio <petruchelban@gmail.com>
2022-06-16 23:58:44 +0100

    Add docstrings to constants

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+"""Constants"""
+# contains column names
 column_names = [
     "ETF",
     "Today",
@@ -7,19 +9,23 @@ column_names = [
     "3 Years"
 ]
 
+# contains the list of tickers for us ets's
 us_etf_tickers = ['XLB', 'XLC', 'XLY',
                   'XLP', 'XLE', 'XLF',
                   'XLV', 'XLI', 'XLK',
                   'XLU', 'XLRE']
 
+# contains the list of tickers for us small-cap ets's
 us_small_etf_tickers = ['PSCD', 'PSCC', 'PSCH',
                         'PSCF', 'PSCE', 'PSCI',
                         'PSCM', 'PSCU', 'PSCT']
 
+# contains the list of tickers for global ets's
 global_etf_tickers = ['MXI', 'JXI', 'RXI',
                       'KXI', 'IXC', 'IXG',
                       'IXJ', 'EXI', 'IXN', 'IXP']
 
+# contains the list of tickers names for us ets's
 us_etf_tickers_names = [
     'Basic Materials',
     'Communication Services',
@@ -34,6 +40,7 @@ us_etf_tickers_names = [
     'Real Estate',
 ]
 
+# contains the list of tickers names for us small-cap ets's
 us_small_etf_tickers_names = [
     'Consumer Discretionary',
     'Consumer Staples',
@@ -46,6 +53,7 @@ us_small_etf_tickers_names = [
     'Technology'
 ]
 
+# contains the list of tickers names for global ets's
 global_etf_tickers_names = [
     'Basic Materials',
     'Utilities',
